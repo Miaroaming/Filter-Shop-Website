167a6473aa7629a65b9fa70d4dae25414967b5e5
Miaroaming <mianzroaming@gmail.com>
2024-05-23 18:20:18 +1200

    finished site

--- script.js
+++ script.js
@@ -1,149 +1,6 @@
-const shoes = [{
+const shoes = [
+{
     id: 1,
-    name: "Old Fashioned Chic Platform Boots",
-    type: "Knee High Boots, Platforms",
-    price: "$255.00",
-    image1: "./images/flowerboots2.webp",
-    image2: "./images/flowerboots.webp",
-    image3: "./images/flowerboots3.webp"
-},
-{
-    id: 2,
-    name: "Sheriff Shine Cowboy Boots - Ziggy",
-    type: "Glitzy, Kooky",
-    price: "$263.00",
-    image1: "./images/ziggy2.webp",
-    image2: "./images/ziggy.webp",
-    image3: "./images/ziggy3.webp"
-},
-{
-    id: 3,
-    name: "Lexus Platform Boots - Blue",
-    type: "Glitzy, Platforms",
-    price: "$145.00",
-    image1: "./images/shinyblue2.webp",
-    image2: "./images/shinyblue.webp",
-    image3: "./images/shinyblue3.webp"
-},
-{
-    id: 4,
-    name: "Blushin' n' Crushin' Marabou Heels - Lime Green",
-    type: "Cutesy, Platforms",
-    price: "$118.00",
-    image1: "./images/greenfluff2.webp.webp",
-    image2: "./images/greenfluff.webp",
-    image3: "./images/greenfluff3.webp.webp"
-},
-{
-    id: 5,
-    name: "Scandalous Seduction Heeled Boots - Black",
-    type: "Knee High Boots",
-    price: "$237.00",
-    image1: "./images/witch1.webp",
-    image2: "./images/witch.webp",
-    image3: "./images/witch3.webp"
-},
-{
-    id: 6,
-    name: "Sharp Sheriff Shine Cowboy Boots - Red",
-    type: "Glitzy, Kooky",
-    price: "$235.00",
-    image1: "./images/redcowboy1.webp",
-    image2: "./images/redcowboy.webp",
-    image3: "./images/redcowboy3.webp"
-},
-{
-    id: 7,
-    name: "Bubblegum Obsidian Pocket Combat Boots",
-    type: "Cutesy, Knee High Boots",
-    price: "$201.00",
-    image1: "./images/pinkcombat2.webp",
-    image2: "./images/pinkcombat1.webp",
-    image3: "./images/pinkcombat3.webp"
-},
-{
-    id: 8,
-    name: "Zuri Metallic Snow Ankle Boots - Silver",
-    type: "Kooky, Cutesy",
-    price: "$40.00",
-    image1: "./images/silverpuff2.webp",
-    image2: "./images/silverpuff.webp",
-    image3: "./images/silverpuff3.webp"
-},
-{
-    id: 9,
-    name: "Metamorphic Butterfly Boots - Silver",
-    type: "Kooky, Cutesy, Glitzy",
-    price: "$300.00",
-    image1: "./images/butterfly2.webp",
-    image2: "./images/butterfly.webp",
-    image3: "./images/butterfly3.webp"
-},
-{
-    id: 10,
-    name: "How Sweet It Is Combat Boots - Black/Pink",
-    type: "Kooky, Cutesy, Platforms",
-    price: "$189.00",
-    image1: "./images/swirl2.webp",
-    image2: "./images/swirl.webp",
-    image3: "./images/swirl3.webp"
-},
-{
-    id: 11,
-    name: "Holy Revelation Platform Heels - Pink",
-    type: "Cutesy, Platforms, Glitzy",
-    price: "$216.00",
-    image1: "./images/pinkchain2.webp",
-    image2: "./images/pinkchain.webp",
-    image3: "./images/pinkchain3.webp"
-},
-{
-    id: 12,
-    name: "Happy Daze Platforms",
-    type: "Kooky, Platforms",
-    price: "$173.00",
-    image1: "./images/rainbow2.webp",
-    image2: "./images/rainbow.webp",
-    image3: "./images/rainbow3.webp"
-},
-{
-    id: 13,
-    name: "Traitor Boots - Pink Fur",
-    type: "Kooky, Platforms, Cutesy",
-    price: "$112.00",
-    image1: "./images/fur2.webp",
-    image2: "./images/fur.webp",
-    image3: "./images/fur3.webp"
-},
-{
-    id: 14,
-    name: "It’s Always Greener Platform Mary Janes",
-    type: "Kooky, Platforms",
-    price: "$96.00",
-    image1: "./images/grass2.webp",
-    image2: "./images/grass.webp",
-    image3: "./images/grass3.webp"
-},
-{
-    id: 15,
-    name: "Glam Life Ankle Boots",
-    type: "Cutesy, Glitzy",
-    price: "$30.00",
-    image1: "./images/purple2.webp",
-    image2: "./images/purple.webp",
-    image3: "./images/purple3.webp"
-},
-{
-    id: 16,
-    name: "Psychedelic Fairytale Double Stack Traitor Boots - White",
-    type: "Cutesy, Kooky, Platforms",
-    price: "$253.00",
-    image1: "./images/mushroom2.webp",
-    image2: "./images/mushroom.webp",
-    image3: "./images/mushroom3.webp"
-},
-{
-    id: 17,
     name: "Love at Every Corner Platform Heels - White",
     type: "Cutesy, Platforms, Glitzy",
     price: "$196.00",
@@ -152,7 +9,7 @@ const shoes = [{
     image3: "./images/blueprint3.webp"
 },
 {
-    id: 18,
+    id: 2,
     name: "Witty Persuaions Platform Sandals",
     type: "Cutesy, Platforms, Glitzy",
     price: "$194.00",
@@ -161,7 +18,7 @@ const shoes = [{
     image3: "./images/blueruffle3.webp"
 },
 {
-    id: 19,
+    id: 3,
     name: "Carefree Muse Platform Sandals - Green",
     type: "Cutesy, Platforms",
     price: "$251.00",
@@ -170,7 +27,7 @@ const shoes = [{
     image3: "./images/limesandals3.webp"
 },
 {
-    id: 20,
+    id: 4,
     name: "Snuggle Wonderland Platform Boots - Pink",
     type: "Cutesy, Platforms",
     price: "$205.00",
@@ -178,7 +35,150 @@ const shoes = [{
     image2: "./images/cutepink.webp",
     image3: "./images/cutepink4.webp"
 },
-
+{
+    id: 5,
+    name: "Old Fashioned Chic Platform Boots",
+    type: "Knee High Boots, Platforms",
+    price: "$255.00",
+    image1: "./images/flowerboots2.webp",
+    image2: "./images/flowerboots.webp",
+    image3: "./images/flowerboots3.webp"
+},
+{
+    id: 6,
+    name: "Sheriff Shine Cowboy Boots - Ziggy",
+    type: "Glitzy, Kooky",
+    price: "$263.00",
+    image1: "./images/ziggy2.webp",
+    image2: "./images/ziggy.webp",
+    image3: "./images/ziggy3.webp"
+},
+{
+    id: 7,
+    name: "Lexus Platform Boots - Blue",
+    type: "Glitzy, Platforms",
+    price: "$145.00",
+    image1: "./images/shinyblue2.webp",
+    image2: "./images/shinyblue.webp",
+    image3: "./images/shinyblue3.webp"
+},
+{
+    id: 8,
+    name: "Blushin' n' Crushin' Marabou Heels - Lime Green",
+    type: "Cutesy, Platforms",
+    price: "$118.00",
+    image1: "./images/greenfluff2.webp.webp",
+    image2: "./images/greenfluff.webp",
+    image3: "./images/greenfluff3.webp.webp"
+},
+{
+    id: 9,
+    name: "Scandalous Seduction Heeled Boots - Black",
+    type: "Knee High Boots",
+    price: "$237.00",
+    image1: "./images/witch1.webp",
+    image2: "./images/witch.webp",
+    image3: "./images/witch3.webp"
+},
+{
+    id: 10,
+    name: "Sharp Sheriff Shine Cowboy Boots - Red",
+    type: "Glitzy, Kooky",
+    price: "$235.00",
+    image1: "./images/redcowboy1.webp",
+    image2: "./images/redcowboy.webp",
+    image3: "./images/redcowboy3.webp"
+},
+{
+    id: 11,
+    name: "Bubblegum Obsidian Pocket Combat Boots",
+    type: "Cutesy, Knee High Boots",
+    price: "$201.00",
+    image1: "./images/pinkcombat2.webp",
+    image2: "./images/pinkcombat1.webp",
+    image3: "./images/pinkcombat3.webp"
+},
+{
+    id: 12,
+    name: "Zuri Metallic Snow Ankle Boots - Silver",
+    type: "Kooky, Cutesy",
+    price: "$40.00",
+    image1: "./images/silverpuff2.webp",
+    image2: "./images/silverpuff.webp",
+    image3: "./images/silverpuff3.webp"
+},
+{
+    id: 13,
+    name: "Metamorphic Butterfly Boots - Silver",
+    type: "Kooky, Cutesy, Glitzy",
+    price: "$300.00",
+    image1: "./images/butterfly2.webp",
+    image2: "./images/butterfly.webp",
+    image3: "./images/butterfly3.webp"
+},
+{
+    id: 14,
+    name: "How Sweet It Is Combat Boots - Black/Pink",
+    type: "Kooky, Cutesy, Platforms",
+    price: "$189.00",
+    image1: "./images/swirl2.webp",
+    image2: "./images/swirl.webp",
+    image3: "./images/swirl3.webp"
+},
+{
+    id: 15,
+    name: "Holy Revelation Platform Heels - Pink",
+    type: "Cutesy, Platforms, Glitzy",
+    price: "$216.00",
+    image1: "./images/pinkchain2.webp",
+    image2: "./images/pinkchain.webp",
+    image3: "./images/pinkchain3.webp"
+},
+{
+    id: 16,
+    name: "Happy Daze Platforms",
+    type: "Kooky, Platforms",
+    price: "$173.00",
+    image1: "./images/rainbow2.webp",
+    image2: "./images/rainbow.webp",
+    image3: "./images/rainbow3.webp"
+},
+{
+    id: 17,
+    name: "Camel-223 Platform Boots - Light Purlpe",
+    type: "Cutesy, Platforms, Knee High Boots",
+    price: "$240.00",
+    image1: "./images/purpleplat1.webp",
+    image2: "./images/purpleplat2.webp",
+    image3: "./images/purpleplat3.webp"
+},
+{
+    id: 18,
+    name: "Traitor Boots - Pink Fur",
+    type: "Kooky, Platforms, Cutesy",
+    price: "$112.00",
+    image1: "./images/fur2.webp",
+    image2: "./images/fur.webp",
+    image3: "./images/fur3.webp"
+},
+{
+    id: 19,
+    name: "Glam Life Ankle Boots",
+    type: "Cutesy, Glitzy",
+    price: "$30.00",
+    image1: "./images/purple2.webp",
+    image2: "./images/purple.webp",
+    image3: "./images/purple3.webp"
+},
+{
+    id: 20,
+    name: "Psychedelic Fairytale Double Stack Traitor Boots - White",
+    type: "Cutesy, Kooky, Platforms",
+    price: "$253.00",
+    image1: "./images/mushroom2.webp",
+    image2: "./images/mushroom.webp",
+    image3: "./images/mushroom3.webp"
+}
 ];
 
 // get input elements 
@@ -352,3 +352,46 @@ if (filteredResults.length === 0) {
     });
 }
 };
+
+
+
+// ------------- MAPBOX ---------------
+mapboxgl.accessToken = 'pk.eyJ1IjoiY2lhcmFuc2xvdyIsImEiOiJjbHY0ZW91YnYwOGV3MmlwOGQ5b3l3a3J3In0.EFWZEAWA13ehFAw5jdLqJA';
+
+//  Initialise your map
+const map = new mapboxgl.Map({
+    container: 'map', // id of the container you are placing the map into
+    style: 'mapbox://styles/mapbox/streets-v11', // style url - set up in the dashboard (streets version)
+    center: [174.78226525319212, -41.27408640732607], // starting position of the map [lng,lat] (remember to flip from google)
+    zoom: 18 // starting zoom level
+});
+
+// set map container dimensions to match its parents
+map.on('load', function() {
+    map.resize();
+})
+
+// Get the marker element from the html:
+const customMarker1 = document.getElementById('custom-marker1');
+
+// Add Markers to your Map:
+new mapboxgl.Marker(customMarker1)
+    .setLngLat([174.78226525319212, -41.27408640732607])
+    // add property details to the marker
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML("<img src='hobson.png' alt=''><h3>Mimi's Shop</h3><p>8 Hobson Street, Thorndon, Wellington</p>"))
+    .addTo(map);
+
+// Fly To
+function flyToLocation(location, zoom) {
+    map.flyTo({
+        center: location,
+        zoom: zoom,
+        essential: true // ignore prefers-reduced-motion
+    });
+}
+
+const shopFlyTo = document.getElementById('fly-to-shop');
+
+shopFlyTo.addEventListener('click', function() {
+    flyToLocation([174.78226525319212, -41.27408640732607], 12);
+})
